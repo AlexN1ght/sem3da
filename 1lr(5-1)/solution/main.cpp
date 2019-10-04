16eede8be65d2c37c53a276016f2a0617136f080
AlexN1ght <AlexN1ght>
2019-10-04 16:12:12 +0300

    Test New CE and replace tabs with spaces

--- 1lr(5-1)/solution/main.cpp
+++ 1lr(5-1)/solution/main.cpp
@@ -4,8 +4,8 @@
 const int MD5_LENGTH = 32;
 
 struct keyVal {
-	char key[33];
-	char value[65];
+    char key[33];
+    char value[65];
 };
 
 int Atoi(char a) {
@@ -15,19 +15,21 @@ int Atoi(char a) {
     return a - '0';
 }
 
+
+
 void CountingSortInDig(TVector <keyVal>& unsorted, const int dig) {
-    int count[16]{0};
-    TVector <keyVal> out(unsorted.Size());
-    for (int i = 0; i <  unsorted.Size(); i++) {
-	    count[Atoi(unsorted[i].key[dig])]++;
-	}
-	for (int i = 1; i < 16; i++) {
-	    count[i] = count[i] + count[i - 1];
-	}
-	for (int i = unsorted.Size() - 1; i >= 0; i--) {
-	    out[--count[Atoi(unsorted[i].key[dig])]] = unsorted[i];
-	}
-	Swap(out, unsorted);
+   int count[16]{0};
+   TVector <keyVal> out(unsorted.Size());
+   for (int i = 0; i <  unsorted.Size(); i++) {
+        count[Atoi(unsorted[i].key[dig])]++;
+    }
+    for (int i = 1; i < 16; i++) {
+        count[i] = count[i] + count[i - 1];
+    }
+    for (int i = unsorted.Size() - 1; i >= 0; i--) {
+        out[--count[Atoi(unsorted[i].key[dig])]] = unsorted[i];
+    }
+    Swap(out, unsorted);
 }
 
 void RadixSort(TVector<keyVal>& unsorted) {
@@ -37,15 +39,16 @@ void RadixSort(TVector<keyVal>& unsorted) {
 }
 
 int main(void) {
-     std::ios::sync_with_stdio(false);
-	 TVector <keyVal> entries;
-	 keyVal tmp;
-	 while(std::cin >> tmp.key >> tmp.value) {
-	    entries.Push_back(tmp);
-	 }
-	 RadixSort(entries);
-	 for (int i = 0; i <  entries.Size(); i++) {
-	    std::cout << entries[i].key << '\t' << entries[i].value << std::endl;
-	 }
-	 return 0;
+    std::ios::sync_with_stdio(false);
+    TVector <keyVal> entries;
+    keyVal tmp;
+    while(std::cin >> tmp.key >> tmp.value) {
+        entries.Push_back(tmp);
+    }
+    RadixSort(entries);
+    for (int i = 0; i <  entries.Size(); i++) {
+        std::cout << entries[i].key << '\t' << entries[i].value << std::endl;
+    }
+    return 0;
 }
+
